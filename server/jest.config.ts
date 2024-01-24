5e6216157dbc8c39230bfb6c6af0f8c8afdd46aa
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-01-24 21:37:18 +0100

    server: Add test setup (#4)

---
+++ server/jest.config.ts
@@ -0,0 +1,17 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  rootDir: '.',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/', '<rootDir>/public'],
+  moduleNameMapper: {
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1'
+  },
+  testRegex: '.spec.ts$',
+  setupFilesAfterEnv: []
+}
+
+export default jestConfig
