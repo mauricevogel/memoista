49807a909f0013be7ca3f1505cbfd480dab4a9a6
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-01-25 10:25:08 +0100

    server: Setup routing-controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,27 @@
 import bodyParser from 'body-parser'
-import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import express from 'express'
 import env from '@config/env'
+import { useExpressServer } from 'routing-controllers'
+import { SampleController } from '@src/controllers/sample.controller'
 
-const app: Express = express()
 const PORT = env.port || 3000
+const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/', (req: Request, res: Response) => {
-  return res.json({ message: 'Hello World!' })
+useExpressServer(app, {
+  controllers: [SampleController],
+  routePrefix: '/api',
+  development: false,
+  defaultErrorHandler: false,
+  validation: {
+    validationError: {
+      target: false
+    }
+  }
 })
 
 app.listen(PORT, async () => {
