9571e77f0a1e470cae633d074ea14728a8ed80b3
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-01-29 13:37:27 +0100

    server: Add signin endpoint

--- server/test/controllers/auth.controller.spec.ts
+++ server/test/controllers/auth.controller.spec.ts
@@ -8,6 +8,22 @@ Container.set(AuthService, AuthServiceMock)
 describe('AuthController', () => {
   const authController = new AuthController()
 
+  describe('POST signin', () => {
+    it('should signin a user', async () => {
+      const signinDto = {
+        email: 'test@example.com',
+        password: 'testpassword'
+      }
+
+      AuthServiceMock.signInWithCredentials.mockResolvedValue('testtoken')
+
+      const response = await authController.signInWithCredentials(signinDto)
+
+      expect(AuthServiceMock.signInWithCredentials).toHaveBeenCalledWith(signinDto)
+      expect(response).toEqual({ accessToken: 'testtoken' })
+    })
+  })
+
   describe('POST register', () => {
     it('should register a user', async () => {
       const registerUserDto = {
