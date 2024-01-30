ed342ed8bfa5cd5fb74e5d69720d7a6b6816ea86
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-01-30 10:01:46 +0100

    client: Add dark mode support

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 
+import { DarkModeToggle } from '@/components/ui/dark-mode-toggle'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <Image src="/logo.svg" alt="Memoista Logo" width={150} height={50} priority />
+      <DarkModeToggle />
     </main>
   )
 }
