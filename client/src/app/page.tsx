0569dadf73c681adb5f869256f2154a867ec9c44
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-02-01 12:53:47 +0100

    client: Setup cypress for e2e testing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Test Heading</h1>
       <Image src="/logo.svg" alt="Memoista Logo" width={150} height={50} priority />
+      <div>
+        <h1>Home</h1>
+        <Link href="/about">About</Link>
+      </div>
     </main>
   )
 }
