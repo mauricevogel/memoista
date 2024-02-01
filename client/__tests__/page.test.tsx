4700786dbd259461b2a11c189a92dbe3baff9215
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-02-01 14:34:13 +0100

    client: Setup mantine as UI library (#18)

--- client/__tests__/page.test.tsx
+++ client/__tests__/page.test.tsx
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom'
 
-import { render, screen } from '@testing-library/react'
-
 import Page from '@/app/page'
+
+import { render, screen } from './utils'
 
 describe('Page', () => {
   it('renders a heading', () => {
