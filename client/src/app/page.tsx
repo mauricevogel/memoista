01793fb3ec6559521bd400dfc4033662bb561c28
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-02-01 13:05:44 +0100

    client: Setup testing (#17)
    * client: Setup jest + testing library
    
    * client: Setup cypress for e2e testing
    
    * client: Update github actions workflow

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image src="/logo.svg" alt="Memoista Logo" width={150} height={50} priority />
+      <div>
+        <h1>Home</h1>
+        <Link href="/about">About</Link>
+      </div>
     </main>
   )
 }
