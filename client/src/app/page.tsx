3385a447dd6754e6b729e245a06d4bd27792f6bf
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-02-01 12:44:02 +0100

    client: Setup jest + testing library

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Test Heading</h1>
       <Image src="/logo.svg" alt="Memoista Logo" width={150} height={50} priority />
     </main>
   )
